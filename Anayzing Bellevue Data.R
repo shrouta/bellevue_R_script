b510df0a41a20c09de69289cce070a7588a57cbe
Anelise Hanson Shrout <shrouta@gmail.com>
2018-07-09 16:27:21 -0700

    Add files via upload

--- Anayzing Bellevue Data.R
+++ Anayzing Bellevue Data.R
@@ -1,10 +1,13 @@
 # Some R scripts for playing around with the Bellevue Almshouse Data.
 
 # Loads necessary libraries
-library("psych")
-library("dplyr")
-library("gmodels")
+install.packages("gmodels")
+library(psych)
+library(dplyr)
+library(gmodels)
 
+# Read in CSV file from Cleaning Bellevue Data.R
+data <- read.csv(file.choose())
 
 # Create age categories
 data$ageord <- cut(data$age_standard, c(0,3,10,18,30,100), labels = c(0:4))
@@ -19,13 +22,21 @@ by_whom_sent_dummy <- as.data.frame(dummy.code(data$admittor_1_cleaned))
 # Creates xtab
 dummies <- data.frame(data$ageord, data$occupation, data$gender, gender_dummy$f, gender_dummy$m, data$reason, data$admittor_1_cleaned, by_whom_sent_dummy, reason_dummy, data$sent_to, sent_to_dummy)
 
-dummies$specific <- dummies$rheumatism + dummies$consumption + dummies$ophthalmia + dummies$rheumanation + dummies$dysentery + dummies$typhus + dummies$typhus.fever + dummies$rhumatic + dummies$erysipelas + dummies$measles + dummies$sciatica + dummies$bronchitis + dummies$scrofula + dummies$rickets + dummies$syphilis + dummies$ulcers + dummies$piles + dummies$rhumanic + dummies$phthisis + dummies$sec.syphilis + dummies$dropsy + dummies$phehius + dummies$trontial + dummies$ustio + dummies$pneumonia + dummies$ague + dummies$exzema + dummies$asthma + dummies$hyperchondria + dummies$abscess + dummies$colic + dummies$opthalamia + dummies$icterus + dummies$diarrhea + dummies$phagadenic.ulcers + dummies$neuralgia + dummies$ascites + dummies$cognetis.cretin + dummies$phentitis + dummies$tumor + dummies$pritchins + dummies$rever + dummies$scarletina + dummies$opthalia + dummies$congested.head + dummies$sea.hove + dummies$coup.de.soleil + dummies$small.pox + dummies$spinal.disease
+# Groups diseases according to type
 
-dummies$injuries <- dummies$lame + dummies$same + dummies$sore.limb + dummies$injury + dummies$spinal.disease + dummies$cripple + dummies$contusion + dummies$throat.cut + dummies$fracture + dummies$sprained.ankle + dummies$fractured.leg + dummies$broken.bone + dummies$severed.limb + dummies$sprained.jaw + dummies$bleeding
+dummies$disability <- dummies$blind + dummies$cripple + dummies$deaf + dummies$disabled + dummies$lame + dummies$same
 
-dummies$mental <- dummies$fits + dummies$ungovernable + dummies$emotional + dummies$delusion.dreams + dummies$horrors + dummies$insane
+dummies$fever <- dummies$fever + dummies$rever
 
-# Creates a table with age categories and entry into the Shanty
+dummies$injury <- dummies$bleeding + dummies$broken.bone + dummies$contusion + dummies$fracture + dummies$fractured.leg + dummies$injury + dummies$severed.limb + dummies$sore.limb + dummies$sprained.ankle + dummies$sprained.jaw + dummies$throat.cut
+
+dummies$mental <- dummies$delusion.dreams + dummies$emotional + dummies$horrors + dummies$hyperchondria + dummies$insane + dummies$non.comps
+
+dummies$social_general <- dummies$abandonment + dummies$debility + dummies$drunkenness + dummies$fits + dummies$from.trial + dummies$intemperance + dummies$old.age + dummies$poorly + dummies$ungovernable
+
+dummies$specific <- dummies$icterus + dummies$abscess + dummies$ague + dummies$ascites + dummies$asthma + dummies$bronchitis + dummies$cognetis.cretin + dummies$colic + dummies$congested.head + dummies$consumption + dummies$diarrhea + dummies$dropsy + dummies$dysentery + dummies$erysipelas + dummies$exzema + dummies$lomehan + dummies$measles + dummies$neuralgia + dummies$ophthalmia + dummies$opthalamia + dummies$opthalia + dummies$phagadenic.ulcers + dummies$phehius + dummies$phentitis + dummies$phthisis + dummies$piles + dummies$pneumonia + dummies$pritchins + dummies$rheumanation + dummies$rheumatism + dummies$rhumanic + dummies$rhumatic + dummies$rickets + dummies$scarletina + dummies$sciatica + dummies$scrofula + dummies$sea.hove + dummies$sec.syphilis + dummies$spinal.disease + dummies$tile + dummies$trontial + dummies$tumor + dummies$typhus + dummies$typhus.fever + dummies$ulcers + dummies$ustio
+
+# Creates a table with age categories and entry into the garret
 garret_age <- xtabs(~ data.ageord + Bellevue_Garret, data = dummies)
 garret_age_reason <- xtabs(~ data.ageord + Bellevue_Garret + data.reason, data = dummies)
 prop.table(garret_age)
@@ -44,43 +55,110 @@ CrossTable(dummies$data.reason, dummies$Bellevue_Shanty, format="SPSS")
 CrossTable(dummies$data.ageord, dummies$Bellevue_Shanty, format="SPSS")
 CrossTable(dummies$data.admittor_1_cleaned, dummies$Bellevue_Shanty, format="SPSS")
 
-# Logistic regression - treating the missing category reference category - intercept stands in as log odds
-shanty_logit <- glm(Bellevue_Shanty ~ specific + injuries + destitution + mental + data.ageord, data=dummies, family="binomial")
+# Logistic regression - treating the missing category reference category - intercept stands in as log odds. Second pass includes only significant variables
+sink(file="recent_emigrant_logistic_output.txt", type="output")
+shanty_logit <- glm(Bellevue_Shanty ~ recent.emigrant + disability + fever + injury + mental + pregnant + sickness + social_general + specific + data.ageord, data=dummies, family="binomial")
 summary(shanty_logit)
 shanty_coef <- shanty_logit$coefficients
 exp(shanty_coef)
 
-chapel_logit <- glm(Bellevue_Chapel ~ specific + injuries + destitution + mental + data.ageord, data=dummies, family="binomial")
+chapel_logit <- glm(Bellevue_Chapel ~ recent.emigrant + disability + fever + injury + mental + pregnant + sickness + social_general + specific + data.ageord, data=dummies, family="binomial")
 summary(chapel_logit)
 chapel_coef <- chapel_logit$coefficients
 exp(chapel_coef)
 
-garret_logit <- glm(Bellevue_Garret ~ specific + injuries + destitution + mental + data.ageord, data=dummies, family="binomial")
+garret_logit <- glm(Bellevue_Garret ~ recent.emigrant + disability + fever + injury + mental + pregnant + sickness + social_general + specific + data.ageord, data=dummies, family="binomial")
 summary(garret_logit)
 garret_coef <- garret_logit$coefficients
 exp(garret_coef)
 
-almshouse_logit <- glm(Bellevue_Almshouse ~ specific + injuries + destitution + mental + data.ageord, data=dummies, family="binomial")
+almshouse_logit <- glm(Bellevue_Almshouse ~ recent.emigrant + disability + fever + injury + mental + pregnant + sickness + social_general + specific + data.ageord, data=dummies, family="binomial")
 summary(almshouse_logit)
 almshouse_coef <- almshouse_logit$coefficients
 exp(almshouse_coef)
 
-blackwell_logit <- glm(Blackwell_NA ~ specific + injuries + destitution + mental + data.ageord, data=dummies, family="binomial")
+blackwell_logit <- glm(Blackwell_NA ~ recent.emigrant + disability + fever + injury + mental + pregnant + sickness + social_general + specific + data.ageord, data=dummies, family="binomial")
 summary(blackwell_logit)
 blackwell_coef <- blackwell_logit$coefficients
 exp(blackwell_coef)
 
-randall_logit <- glm(Randall_NA ~ specific + injuries + destitution + mental + data.ageord, data=dummies, family="binomial")
+randall_logit <- glm(Randall_NA ~ recent.emigrant + disability + fever + injury + mental + pregnant + sickness + social_general + specific + data.ageord, data=dummies, family="binomial")
 summary(randall_logit)
 randall_coef <- randall_logit$coefficients
 exp(randall_coef)
 
-hospital_logit <- glm(Bellevue_Hospital ~ specific + injuries + destitution + mental + data.ageord, data=dummies, family="binomial")
+hospital_logit <- glm(Bellevue_Hospital ~ recent.emigrant + disability + fever + injury + mental + pregnant + sickness + social_general + specific + data.ageord, data=dummies, family="binomial")
 summary(hospital_logit)
 hospital_coef <- hospital_logit$coefficients
 exp(hospital_coef)
 
-recent_emigrant_logit <- glm(recent.emigrant ~ Bellevue_Shanty + Bellevue_Garret + Bellevue_Hospital + Long_Island_NA + Lunatic_Asylum_NA + Randall_NA + Blackwell_NA + data.ageord, data=dummies, family="binomial")
-summary(recent_emigrant_logit)
-recent_emigrant_coef <- recent_emigrant_logit$coefficients
-exp(recent_emigrant_coef)
+lunatic_logit <- glm(Lunatic_Asylum_NA ~ recent.emigrant + disability + fever + injury + mental + pregnant + sickness + social_general + specific + data.ageord, data=dummies, family="binomial")
+summary(lunatic_logit)
+lunatic_coef <- lunatic_logit$coefficients
+exp(lunatic_coef)
+
+long_island_logit <- glm(Long_Island_NA ~ recent.emigrant + disability + fever + injury + mental + pregnant + sickness + social_general + specific + data.ageord, data=dummies, family="binomial")
+summary(long_island_logit)
+long_island_coef <- long_island_logit$coefficients
+exp(long_island_coef)
+
+shanty_admittor_logit <- glm(Bellevue_Shanty ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(shanty_admittor_logit)
+shanty_admittor_coef <- shanty_admittor_logit$coefficients
+exp(shanty_admittor_coef)
+
+chapel_logit <- glm(Bellevue_Chapel ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(chapel_logit)
+chapel_coef <- chapel_logit$coefficients
+exp(chapel_coef)
+
+garret_admittor_logit <- glm(Bellevue_Garret ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(garret_admittor_logit)
+garret_admittor_coef <- garret_admittor_logit$coefficients
+exp(garret_admittor_coef)
+
+almshouse_admittor_logit <- glm(Bellevue_Almshouse ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(almshouse_admittor_logit)
+almshouse_admittor_coef <- almshouse_admittor_logit$coefficients
+exp(almshouse_admittor_coef)
+
+blackwell_admittor_logit <- glm(Blackwell_NA ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(blackwell_admittor_logit)
+blackwell_admittor_coef <- blackwell_admittor_logit$coefficients
+exp(blackwell_admittor_coef)
+
+randall_admittor_logit <- glm(Randall_NA ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(randall_admittor_logit)
+randall_admittor_coef <- randall_admittor_logit$coefficients
+exp(randall_admittor_coef)
+
+hospital_admittor_logit <- glm(Bellevue_Hospital ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(hospital_admittor_logit)
+hospital_admittor_coef <- hospital_admittor_logit$coefficients
+exp(hospital_admittor_coef)
+
+lunatic_admittor_logit <- glm(Lunatic_Asylum_NA ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(lunatic_admittor_logit)
+lunatic_admittor_coef <- lunatic_admittor_logit$coefficients
+exp(lunatic_admittor_coef)
+
+long_island_admittor_logit <- glm(Long_Island_NA ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(long_island_admittor_logit)
+long_island_admittor_coef <- long_island_admittor_logit$coefficients
+exp(long_island_admittor_coef)
+
+recent_emigrant_site_logit <- glm(recent.emigrant ~ Bellevue_Shanty + Bellevue_Garret + Bellevue_Hospital + Long_Island_NA + Lunatic_Asylum_NA + Randall_NA + Blackwell_NA + data.ageord, data=dummies, family="binomial")
+summary(recent_emigrant_site_logit)
+recent_emigrant_site_coef <- recent_emigrant_site_logit$coefficients
+exp(recent_emigrant_site_coef)
+
+recent_emigrant_admittor_logit <- glm(recent.emigrant ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(recent_emigrant_admittor_logit)
+recent_emigrant_admittor_coef <- recent_emigrant_admittor_logit$coefficients
+exp(recent_emigrant_admittor_coef)
+
+destitution_admittor_logit <- glm(destitution ~ a.b.roberts + a.g..schratty + abraham.r..lawrence + ald..pan + alexander.h..schultz + benson.s..hopkins + bernard.j..mersole + clarkson.crolius + commissioners.of.emigration + edward.witherell + g.w..anderson + g.w..hansen.4th.ward + george.h..purser + illegibile + j..blackwell + jacob.l..dodge + james.d..oliver + james.donnelly + james.kelly + james.walsh + john.p..cumming + leonard.l..johnson + mcgavin + morris.franklin + moses.g..leonard + moses.m..s..jackson + moses.maynard + oscar.s..field + peter.greg + police + resident.physician + richard.t..compton + superintendent + theodore.r..deforest + thomas.conner + thomas.spofford + washington.smith + william.a..walker + william.adams + william.f..jackson + william.w..fream + data.ageord, data=dummies, family="binomial")
+summary(destitution_admittor_logit)
+destitution_admittor_coef <- destitution_admittor_logit$coefficients
+exp(destitution_admittor_coef)
+sink()
--- Cleaning Bellevue Data.R
+++ Cleaning Bellevue Data.R
@@ -1,43 +1,70 @@
 # Load Libraries
+install.packages("psych")
+install.packages("jsonlite")
 library(readr)
 library(plyr)
-install.packages("psych")
 library(psych)
+library(jsonlite)
 
-# Load and clean admissions data - transcriptions from the Bellevue Almshouse admissions ledgers
-admissions <- read_csv("~/Dropbox (Personal)/Publications/In Progress/Digital Almshouse/Digital almshouse dataset/Bellevue Admissions Records/bellevue_admissions_2018_05_28.csv")
-admissions <- rename(admissions, c(`Reason_cleaned 1`="reason_cleaned"))
+# Read in 'raw' admissions file- transcriptions from the Bellevue Almshouse admissions ledgers
+admissions <- read.csv(file.choose(), na.strings="")
+
+# Clean admissions data
+admissions <- rename(admissions, c(`Reason_cleaned.1`="reason_cleaned"))
 admissions$Age_standardized <- as.integer(admissions$Age_standardized)
 admissions$Date <- as.Date(admissions$Date, "%m/%d/%Y")
-admissions <- rename(admissions, c("Date"="date_in", "Sex"="gender", "Age_standardized"="age_standard", "Occupation"="occupation", "ByWhomSent_1"="admittor_1","ByWhomSent_2"="admittor_2","Remarks"="obligations", "SentTo_cleaned"="sent_to_cleaned"))
+admissions <- rename(admissions, c("Date"="date_in", "Sex"="gender", "Age_standardized"="age_standard", "Occupation"="occupation", "ByWhomSent_1"="admittor_1","ByWhomSent_2"="admittor_2", "SentTo_cleaned"="sent_to_cleaned"))
 
 # Load and clean sites data - dataset constructed from the places referenced in the Bellevue Almshouse admissions ledgers
-sites <- read_csv("~/Dropbox (Personal)/Publications/In Progress/Digital Almshouse/Digital almshouse dataset/Bellevue sites only/sites.csv")
+sites <-read.csv(file.choose(), na.strings="")
 bellevue_sites_variables <- c("code", "public_health_site", "public_health_site_locale")
 sites <- sites[bellevue_sites_variables]
-sites <- rename(sites, c("public_health_site"="site", "public_health_site_locale"="sub_site"))
-sites$sent_to <- paste0(as.character(sites$site),"_",as.character(sites$sub_site))
+sites <- rename(sites, c("public_health_site"="site", "public_health_site_locale"="institution"))
+sites$sent_to <- paste0(as.character(sites$site),"_",as.character(sites$institution))
+sites$sent_to <- sub("_$", "", sites$sent_to)
 
 # Load and clean admittors data - dataset constructed from the people mentioned in the "By Whom Sent" column of the Bellevue Almshouse admissions ledgers
-admittors <- read_csv("~/Dropbox (Personal)/Publications/In Progress/Digital Almshouse/Digital almshouse dataset/Bellevue admittors only/admittors.csv")
+admittors <- read.csv(file.choose(), na.strings="")
 
 # Merge for admittor 1, 2 and site
 bellevue_admittors1 <- merge(admissions, admittors, by.x="admittor_1", by.y="admittors_from_almshouse_register", all.x=TRUE, all.y=FALSE)
-bellevue_admittors1_variables <- c("LastName", "FirstName", "date_in", "age_standard", "gender", "occupation", "reason_cleaned", "admittor_1", "identified_admittors", "admittor_2", "sent_to_cleaned", "obligations")
+bellevue_admittors1_variables <- c("date_in", "age_standard", "gender", "occupation", "reason_cleaned", "admittor_1", "identified_admittors", "admittor_2", "sent_to_cleaned")
 bellevue_admittors1 <- bellevue_admittors1[bellevue_admittors1_variables]
 bellevue_admittors1 <- rename(bellevue_admittors1, c("identified_admittors"="admittor_1_cleaned"))
 bellevue_admittors2 <- merge(bellevue_admittors1, admittors, by.x="admittor_2", by.y="admittors_from_almshouse_register", all.x=TRUE, all.y=FALSE)
-bellevue_admittors2_variables <- c("LastName", "FirstName", "date_in", "age_standard", "gender", "occupation", "reason_cleaned", "admittor_1", "admittor_1_cleaned", "admittor_2","identified_admittors", "sent_to_cleaned", "obligations")
+bellevue_admittors2_variables <- c("date_in", "age_standard", "gender", "occupation", "reason_cleaned", "admittor_1", "admittor_1_cleaned", "admittor_2","identified_admittors", "sent_to_cleaned")
 bellevue_admittors2 <- bellevue_admittors2[bellevue_admittors2_variables]
 bellevue_admittors2 <- rename(bellevue_admittors2, c("identified_admittors"="admittor_2_cleaned"))
 bellevue_admittors_sites <- merge(bellevue_admittors2, sites, by.x="sent_to_cleaned", by.y="code", all.x=TRUE, all.y=FALSE)
-bellevue_admittors_sites_variables <- c("LastName", "FirstName", "date_in", "age_standard", "gender", "occupation", "reason_cleaned", "admittor_1", "admittor_1_cleaned", "admittor_2","admittor_2_cleaned","obligations","sent_to_cleaned", "site","sub_site","sent_to")
+bellevue_admittors_sites_variables <- c("date_in", "age_standard", "gender", "occupation", "reason_cleaned", "admittor_1_cleaned", "admittor_2_cleaned", "site","institution","sent_to")
 bellevue_admittors_sites <- bellevue_admittors_sites[bellevue_admittors_sites_variables]
+data <- bellevue_admittors_sites
+data$occupation <- as.character(data$occupation)
+data$gender <- as.character(data$gender)
+data$reason_cleaned <- as.character(data$reason_cleaned)
+data$admittor_1_cleaned <- as.character(data$admittor_1_cleaned)
+data$admittor_2_cleaned <- as.character(data$admittor_2_cleaned)
+data$site <- as.character(data$site)
+data$institution <- as.character(data$institution)
+
+# Subset the data to keep only the entries that include "sent_to", removes illegible entries
+data <- data[ which(data$reason_cleaned!="(illegible)"), ]
+data <- data[ which(data$reason_cleaned!="for say"), ]
 
 write.csv(bellevue_admittors_sites, file="bellevue_for_R.csv")
 
-# Subset the data to keep only the entries that include "sent_to", removes illegible entries
-data <- bellevue_admittors_sites[ which(bellevue_admittors_sites$sent_to>0), ]
-data <- data[ which(data$reason_cleaned!="(illegible)"), ]
-data <- data[ which(data$reason_cleaned!="for say"), ]
-data <- data[ which(data$sent_to!="NA_NA"), ]
+# Creates JSON for path visualization
+parallel_vars <- c("gender", "age_category", "occupation", "admittor_1_cleaned", "admittor_2_cleaned", "reason_cleaned", "site", "institution")
+data$age_category <- cut(data$age_standard, c(0,3,10,18,30,100), labels = c("infant", "child", "teen", "young adult", "adult"))
+parallel_data <- data[parallel_vars]
+parallel_data <- rename(parallel_data, c("age_category"="age", "reason_cleaned"="reason", "admittor_1_cleaned"="By_Whom_Sent_1", "admittor_2_cleaned"="By_Whom_Sent_2"))
+parallel_data <- parallel_data[ which(parallel_data$site!="NA"), ]
+parallel_data <- parallel_data[ which(parallel_data$age!="NA"), ]
+parallel_data <- parallel_data[ which(parallel_data$gender!="?"), ]
+parallel_data <- parallel_data[ which(parallel_data$By_Whom_Sent_1!="NA"), ]
+parallel_data[is.na(parallel_data)] <- "not specificed"
+
+Bellevue_JSON <- toJSON(parallel_data, pretty=TRUE)
+write(Bellevue_JSON, file="Bellevue.json")
+
+write.csv(parallel_data, file="Bellevue.csv")
